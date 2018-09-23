ff202ae5caddbc3db668af2e2ad4047fe024eca6
FUSION\jorge.castro <jorge.castro@habitaclia.com>
2018-09-23 22:44:30 +0200

    Refactor API, last_prices, all_prices

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const moment = require('moment');
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.get('/prices', async (req, res) => {   
+app.get('/last_prices', async (req, res) => {   
     const db = repo.openDatabaseConnection();
 
     let sql = `SELECT id as id 
@@ -25,6 +25,30 @@ app.get('/prices', async (req, res) => {
             FROM prices_history
             WHERE price > -1
             GROUP BY room, indate, price_type, adults, children, child_age1, child_age2, child_age3`;
+
+      db.all(sql, function(err, rows) {
+        res.json(rows);
+    });
+      
+    repo.closeDatabaseConnection(db);
+
+});
+
+app.get('/all_prices', async (req, res) => {   
+    const db = repo.openDatabaseConnection();
+
+    let sql = `SELECT id as id 
+                ,room as room
+                ,inDate as inDate
+                ,price_type as price_type
+                ,price as price
+                ,adults as adults
+                ,children as children
+                ,child_age1 as child_age1
+                ,child_age2 as child_age2
+                ,child_age3 as child_age3
+                ,tracked_date as tracked_date
+            FROM prices_history`;
 
       db.all(sql, function(err, rows) {
         res.json(rows);
